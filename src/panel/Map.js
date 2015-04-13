8cfa38c5fd150eba4845a8899fa82084195f2057
Kai Volland <kaivolland@yahoo.de>
2015-04-13 16:11:28 +0200

    Initial upload of a hello-world panel

---
+++ src/panel/Map.js
@@ -0,0 +1,11 @@
+
+Ext.define("GeoExt.panel.Map",{
+    extend: "Ext.panel.Panel",
+
+    controller: "panel-map",
+    viewModel: {
+        type: "panel-map"
+    },
+
+    html: "Hello, World!!"
+});
---
+++ src/panel/MapController.js
@@ -0,0 +1,5 @@
+Ext.define('GeoExt.panel.MapController', {
+    extend: 'Ext.app.ViewController',
+    alias: 'controller.panel-map'
+    
+});
