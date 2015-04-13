f35371875d581a943a38f417c39e18445fffc630
Kai Volland <kaivolland@yahoo.de>
2015-04-13 16:56:29 +0200

    Added config and constructor
    This approach uses the config-object which gifts us getter and setter for the properties. The constructor iterates over the passed mapConf and sets the given values.

--- src/panel/Map.js
+++ src/panel/Map.js
@@ -1,11 +1,39 @@
-
 Ext.define("GeoExt.panel.Map",{
     extend: "Ext.panel.Panel",
+    xtype: "gx_mappanel",
 
     controller: "panel-map",
+
     viewModel: {
         type: "panel-map"
     },
 
-    html: "Hello, World!!"
+    config: {
+
+        center: null,
+
+        extent: null,
+
+        layers: null,
+
+        map: null,
+
+        mapRendered: false,
+
+        zoom: null
+
+    },
+
+    html: "This is a mappanel!!",
+
+    layout: 'fit',
+
+    constructor: function(mapConf){
+        if(!Ext.isempty(mapConf)){
+            Ext.Object.each(mapConf, function(k,v){
+                this[k] = v;
+            })
+        }
+    }
+
 });
