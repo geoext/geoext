a0cbd1601d8a2fba6ff786467fbd48645a0bb6b6
Marc Jansen <jansen@terrestris.de>
2015-05-06 16:54:29 +0200

    Enhance and extend the treepanel examples
    This changes the layout of the basic tree example.
    
    Additionally another example showing two ways of how to add legends to
    trees are added.

--- examples/tree/panel.js
+++ examples/tree/panel.js
@@ -4,60 +4,92 @@ Ext.require([
     'GeoExt.data.TreeStore'
 ]);
 
-Ext.onReady(function(){
-    var source,
-        layer,
-        map,
-        zoomslider;
+var mapPanel,
+    treePanel;
 
-    source1 = new ol.source.MapQuest({layer: 'sat'});
-    layer1 = new ol.layer.Tile({
-      source: source1
-    });
-
-    source2 = new ol.source.MapQuest({layer: 'osm'});
-    layer2 = new ol.layer.Tile({
-      source: source2
-    });
-
-    source3 = new ol.source.MapQuest({layer: 'hyb'});
-    layer3 = new ol.layer.Tile({
-      source: source3
-    });
-
-    group = new ol.layer.Group({
-        layers: [layer1,layer2]
-    });
-
-    olMap = new ol.Map({
-        layers: [
-                 group,
-                 layer3],
-        view: new ol.View({
-          center: [0, 0],
-          zoom: 2
-        })
-    });
-
-    mapPanel = Ext.create('GeoExt.panel.Map', {
-        title: 'GeoExt.tree.Panel Example',
-        width: 800,
-        height: 600,
-        map: olMap,
-        renderTo: 'mapDiv'
-    });
-
-    treeStore = Ext.create('GeoExt.data.TreeStore', {
-        model: 'GeoExt.data.TreeModel',
-        layerStore: mapPanel.getStore()
-    });
-
-    treePanel = Ext.create('GeoExt.tree.Panel', {
-        title: 'treePanel',
-        width: 400,
-        height: 600,
-        store: treeStore,
-        renderTo: 'treeDiv',
-        rootVisible: false
-    });
+Ext.application({
+    name: 'BasicTree',
+    launch: function(){
+        var source1, source2, source3,
+            layer1, layer2, layer3,
+            group,
+            olMap,
+            treeStore;
+    
+        source1 = new ol.source.MapQuest({layer: 'sat'});
+        layer1 = new ol.layer.Tile({
+            source: source1,
+            name: 'MapQuest Satellite'
+        });
+    
+        source2 = new ol.source.MapQuest({layer: 'osm'});
+        layer2 = new ol.layer.Tile({
+            source: source2,
+            name: 'MapQuest OSM'
+        });
+    
+        source3 = new ol.source.MapQuest({layer: 'hyb'});
+        layer3 = new ol.layer.Tile({
+            source: source3,
+            name: 'MapQuest Hybrid'
+        });
+    
+        group = new ol.layer.Group({
+            layers: [layer1,layer2]
+        });
+    
+        olMap = new ol.Map({
+            layers: [group, layer3],
+            view: new ol.View({
+              center: [0, 0],
+              zoom: 2
+            })
+        });
+    
+        mapPanel = Ext.create('GeoExt.panel.Map', {
+            map: olMap,
+            region: 'center',
+            border: false
+        });
+    
+        treeStore = Ext.create('GeoExt.data.TreeStore', {
+            layerStore: mapPanel.getStore()
+        });
+    
+        treePanel = Ext.create('GeoExt.tree.Panel', {
+            title: 'GeoExt.tree.Panel Example',
+            store: treeStore,
+            rootVisible: false,
+            flex: 1,
+            border: false
+        });
+        
+        var description = Ext.create('Ext.panel.Panel', {
+            contentEl: 'description',
+            title: 'Description',
+            height: 200,
+            border: false,
+            bodyPadding: 5
+        });
+        
+        Ext.create('Ext.Viewport', {
+            layout: "border",
+            items:[
+                mapPanel,
+                {
+                    xtype: 'panel',
+                    region: 'west',
+                    width: 400,
+                    layout: {
+                        type: 'vbox',
+                        align: 'stretch'
+                    },
+                    items: [
+                        treePanel,
+                        description
+                    ]
+                }
+            ]
+        });
+    }
 });
