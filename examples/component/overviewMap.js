dde7cd95749f9a131546f8df48c4c22073e69b5e
Marc Jansen <jansen@terrestris.de>
2015-05-07 09:45:07 +0200

    Enhance and extend the overviewmap example

--- examples/component/overviewMap.js
+++ examples/component/overviewMap.js
@@ -3,76 +3,117 @@ Ext.require([
     'GeoExt.component.OverviewMap'
 ]);
 
-Ext.onReady(function(){
-    var source,
-        layer,
-        map,
-        zoomslider;
+var mapPanel,
+    overviewMap1,
+    overviewMap2;
 
-    source = new ol.source.MapQuest({layer: 'sat'});
-    layer = new ol.layer.Tile({
-      source: source
-    });
+Ext.application({
+    name: 'OverviewMaps',
+    launch: function(){
+        var source, source2,
+            layer, layer2,
+            olMap,
+            description,
+            ovMapPanel1, ovMapPanel2;
 
-    source2 = new ol.source.MapQuest({layer: 'osm'});
-    layer2 = new ol.layer.Tile({
-      source: source2
-    });
+        source = new ol.source.MapQuest({layer: 'sat'});
+        layer = new ol.layer.Tile({
+          source: source
+        });
 
-    olMap = new ol.Map({
-        layers: [layer],
-        interactions: ol.interaction.defaults().extend([
-            new ol.interaction.DragRotateAndZoom()
-        ]),
-        view: new ol.View({
-          center: [0, 0],
-          zoom: 2
-        })
-    });
+        source2 = new ol.source.MapQuest({layer: 'osm'});
+        layer2 = new ol.layer.Tile({
+          source: source2
+        });
 
-    mapPanel = Ext.create('GeoExt.panel.Map', {
-        title: 'GeoExt.component.OverviewMap Example',
-        width: 800,
-        height: 600,
-        map: olMap,
-        renderTo: 'mapDiv'
-    });
-
-    overviewMap = Ext.create('GeoExt.component.OverviewMap', {
-        parentMap: olMap,
-        magnification: 10,
-        layers: [layer2],
-        anchorStyle: new ol.style.Style({
-            fill: new ol.style.Fill({
-                color: 'rgba(255, 255, 255, 0.2)'
-              }),
-              stroke: new ol.style.Stroke({
-                color: '#ffcc33',
-                width: 2
-              }),
-              image: new ol.style.Circle({
-                radius: 7,
-                fill: new ol.style.Fill({
-                  color: '#ffcc33'
-                })
-              })
-        }),
-        boxStyle: new ol.style.Style({
-            stroke: new ol.style.Stroke({
-                color: '#ffcc33',
-                width: 2
+        olMap = new ol.Map({
+            layers: [layer],
+            interactions: ol.interaction.defaults().extend([
+                new ol.interaction.DragRotateAndZoom()
+            ]),
+            view: new ol.View({
+              center: [0, 0],
+              zoom: 4
             })
-        })
-    });
+        });
 
-    extPanel = Ext.create('Ext.panel.Panel', {
-        title: 'OverviewMap in Panel',
-        width: 400,
-        height: 200,
-        layout: 'fit',
-        items: [
-            overviewMap
-        ],
-        renderTo: 'panelDiv'
-    });
+        mapPanel = Ext.create('GeoExt.panel.Map', {
+            title: 'GeoExt.component.OverviewMap Example',
+            map: olMap,
+            region: 'center',
+            border: false
+        });
+
+        overviewMap1 = Ext.create('GeoExt.component.OverviewMap', {
+            parentMap: olMap
+        });
+
+        overviewMap2 = Ext.create('GeoExt.component.OverviewMap', {
+            parentMap: olMap,
+            magnification: 12,
+            layers: [layer2],
+            anchorStyle: new ol.style.Style({
+                image: new ol.style.Circle({
+                    radius: 7,
+                    fill: new ol.style.Fill({
+                        color: 'rgb(255, 204, 51)'
+                    })
+                })
+            }),
+            boxStyle: new ol.style.Style({
+                stroke: new ol.style.Stroke({
+                    color: 'rgb(255, 204, 51)',
+                    width: 3
+                }),
+                fill: new ol.style.Fill({
+                    color: 'rgba(255, 204, 51, 0.2)'
+                })
+            })
+        });
+
+        description = Ext.create('Ext.panel.Panel', {
+            contentEl: 'description',
+            title: 'Description',
+            flex: 1,
+            border: false,
+            bodyPadding: 5,
+            autoScroll: true
+        });
+
+        ovMapPanel1 = Ext.create('Ext.panel.Panel', {
+            title: 'OverviewMap (default)',
+            flex: 1,
+            layout: 'fit',
+            items: overviewMap1
+        });
+
+        ovMapPanel2 = Ext.create('Ext.panel.Panel', {
+            title: 'OverviewMap (configured)',
+            flex: 1,
+            layout: 'fit',
+            items: overviewMap2
+        });
+
+        Ext.create('Ext.Viewport', {
+            layout: "border",
+            items:[
+                mapPanel,
+                {
+                    xtype: 'panel',
+                    region: 'east',
+                    width: 400,
+                    border: false,
+                    layout: {
+                        type: 'vbox',
+                        align: 'stretch'
+                    },
+                    items: [
+                        ovMapPanel1,
+                        description,
+                        ovMapPanel2
+                    ]
+                }
+            ]
+        });
+    }
 });
