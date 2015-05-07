880a8a287d3582e0d25046d0198db350dc7bfa49
Marc Jansen <jansen@terrestris.de>
2015-05-07 09:37:54 +0200

    Refactor and enhance the overview component
    The overviewmap component had some issues, which this commit tries to fix:
    
    * We were using hardcoded ides of DOM-elements instead of a generic controller
      that handles rendering and updating.
    * We were instantiating ol-object when the class was constructed, so that
      multiple instances of the overviewmap would all work on shared instances.
    * The above point also meant that we could only have one instance of
      the component since an OpenLayers assertion falure would be raised once you
      instanciate multiple instances.
    
    The controller of the component now is actually being called and mainly does
    the same thing that the MapController does, the first time rendering of the
    ol.Map and also subsequent updates when the window is resized.

--- src/component/OverviewMapController.js
+++ src/component/OverviewMapController.js
@@ -2,12 +2,16 @@ Ext.define('GeoExt.component.OverviewMapController', {
     extend: 'Ext.app.ViewController',
     alias: 'controller.component-overviewmap',
 
-    afterRender: function(){
-        var overviewMapComponent = this.getView(),
-            div = overviewMapComponent.getEl().dom,
-            map = overviewMapComponent.getMap();
-
-        map.setTarget(div);
+    onResize: function(){
+        // Get the corresponding view of the controller (the mapPanel).
+        var overview = this.getView(),
+            div = overview.getEl().dom,
+            map = overview.getMap();
+        if(!overview.mapRendered){
+            map.setTarget(div);
+            overview.mapRendered = true;
+        } else {
+            overview.getMap().updateSize();
+        }
     }
-
 });
