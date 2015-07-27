173029848b4eef302236c5c427bf181cc1a5c703
Marc Jansen <jansen@terrestris.de>
2015-07-27 16:57:49 +0200

    Add tests verifying the filtering behaviour

--- test/spec/GeoExt/data/store/Tree.test.js
+++ test/spec/GeoExt/data/store/Tree.test.js
@@ -1,6 +1,61 @@
 Ext.Loader.syncRequire(['GeoExt.data.store.Tree', 'GeoExt.tree.Panel']);
 
 describe('GeoExt.data.store.Tree', function() {
+
+
+    var div;
+    var olMap;
+    var mapComponent;
+    var source;
+    var layer;
+    var treeStore;
+
+    beforeEach(function(){
+        div = document.createElement('div');
+        div.style.position = "absolute";
+        div.style.top = "0";
+        div.style.left = "-1000px";
+        div.style.width = "512px";
+        div.style.height = "256px";
+        document.body.appendChild(div);
+
+        source = new ol.source.MapQuest({layer: 'sat'});
+        layer = new ol.layer.Tile({
+            source: source
+        });
+
+        olMap = new ol.Map({
+            target: div,
+            layers: [layer],
+            view: new ol.View({
+                center: [0, 0],
+                zoom: 2
+            })
+        });
+
+        mapComponent = Ext.create('GeoExt.component.Map', {
+            map: olMap
+        });
+
+        treeStore = Ext.create('GeoExt.data.store.Tree', {
+            layerGroup: olMap.getLayerGroup()
+        });
+
+        Ext.create('GeoExt.tree.Panel', {
+            title: 'GeoExt.tree.Panel Example',
+            store: treeStore,
+            rootVisible: false,
+            flex: 1,
+            border: false
+        });
+    });
+
+    afterEach(function(){
+        mapComponent.destroy();
+        treeStore.destroy();
+        document.body.removeChild(div);
+    });
+
     describe('basics', function(){
         it('is defined', function(){
             expect(GeoExt.data.store.Tree).not.to.be(undefined);
@@ -8,50 +63,6 @@ describe('GeoExt.data.store.Tree', function() {
     });
 
     describe('Instanciation', function() {
-        var div;
-        var olMap;
-        var source;
-        var layer;
-        var treeStore;
-
-        beforeEach(function(){
-            div = document.createElement('div');
-            document.body.appendChild(div);
-
-            source = new ol.source.MapQuest({layer: 'sat'});
-            layer = new ol.layer.Tile({
-                source: source
-            });
-
-            olMap = new ol.Map({
-                target: div,
-                layers: [layer],
-                view: new ol.View({
-                    center: [0, 0],
-                    zoom: 2
-                })
-            });
-
-            Ext.create('GeoExt.component.Map', {
-                map: olMap
-            });
-
-            treeStore = Ext.create('GeoExt.data.store.Tree', {
-                layerGroup: olMap.getLayerGroup()
-            });
-
-            Ext.create('GeoExt.tree.Panel', {
-                title: 'GeoExt.tree.Panel Example',
-                store: treeStore,
-                rootVisible: false,
-                flex: 1,
-                border: false
-            });
-        });
-
-        afterEach(function(){
-            document.body.removeChild(div);
-        });
 
         it('can be created with ol.Map.getLayerGroup()', function() {
             treeStore = Ext.create('GeoExt.data.store.Tree', {
@@ -80,52 +91,9 @@ describe('GeoExt.data.store.Tree', function() {
         });
 
     });
+
+
     describe('Listens to Map/LayerCollection events', function() {
-        var div;
-        var olMap;
-        var mapComponent;
-        var source;
-        var layer;
-        var treeStore;
-
-        beforeEach(function(){
-            div = document.createElement('div');
-            document.body.appendChild(div);
-
-            source = new ol.source.MapQuest({layer: 'sat'});
-            layer = new ol.layer.Tile({
-                source: source
-            });
-
-            olMap = new ol.Map({
-                target: div,
-                layers: [layer],
-                view: new ol.View({
-                    center: [0, 0],
-                    zoom: 2
-                })
-            });
-
-            mapComponent = Ext.create('GeoExt.component.Map', {
-                map: olMap
-            });
-
-            treeStore = Ext.create('GeoExt.data.store.Tree', {
-                layerGroup: olMap.getLayerGroup()
-            });
-
-            Ext.create('GeoExt.tree.Panel', {
-                title: 'GeoExt.tree.Panel Example',
-                store: treeStore,
-                rootVisible: false,
-                flex: 1,
-                border: false
-            });
-        });
-
-        afterEach(function(){
-            document.body.removeChild(div);
-        });
 
         it('sets add/remove eventlisteners for ol.Collections (hidden LayerGroupNode)', function() {
             treeStore = Ext.create('GeoExt.data.store.Tree', {
@@ -188,6 +156,99 @@ describe('GeoExt.data.store.Tree', function() {
             expect(treeNode.get('text')).to.be(layer2.get('name'));
         });
 
+        describe('Filterable store', function(){
+            var noVectorTreeStore;
+            var noVectorTreePanel;
+
+            beforeEach(function(){
+                noVectorTreeStore = Ext.create('GeoExt.data.store.Tree', {
+                    layerGroup: olMap.getLayerGroup(),
+                    filters: [
+                        function(rec){
+                            return !(rec.data instanceof ol.layer.Vector);
+                        }
+                    ]
+                });
+
+                noVectorTreePanel = Ext.create('GeoExt.tree.Panel', {
+                    store: noVectorTreeStore,
+                    rootVisible: false
+                });
+            });
+
+            afterEach(function(){
+                noVectorTreeStore.destroy();
+                noVectorTreePanel.destroy();
+
+            });
+
+            it('works when the root is collapsed', function(){
+                expect(noVectorTreeStore.getCount()).to.be(1);
+                expect(olMap.getLayers().getLength()).to.be(1);
+
+                // add layer that should be filtered out
+                olMap.addLayer(new ol.layer.Vector());
+
+                expect(noVectorTreeStore.getCount()).to.be(1);
+                expect(olMap.getLayers().getLength()).to.be(2);
+            });
+
+            it('works when the root is expanded', function(){
+                expect(noVectorTreeStore.getCount()).to.be(1);
+                expect(olMap.getLayers().getLength()).to.be(1);
+
+                noVectorTreeStore.getRoot().expand();
+
+                // add layer that should be filtered out
+                olMap.addLayer(new ol.layer.Vector());
+
+                expect(noVectorTreeStore.getCount()).to.be(1);
+                expect(olMap.getLayers().getLength()).to.be(2);
+            });
+
+            it('works when the filter is removed', function(){
+                expect(noVectorTreeStore.getCount()).to.be(1);
+                expect(olMap.getLayers().getLength()).to.be(1);
+
+                // add layer that should be filtered out
+                olMap.addLayer(new ol.layer.Vector());
+
+                expect(noVectorTreeStore.getCount()).to.be(1);
+                expect(olMap.getLayers().getLength()).to.be(2);
+
+                // remove all filters
+                var filters = noVectorTreeStore.getFilters();
+                filters.each(function(filter){
+                    noVectorTreeStore.removeFilter(filter);
+                });
+
+                expect(noVectorTreeStore.getCount()).to.be(2);
+                expect(olMap.getLayers().getLength()).to.be(2);
+            });
+
+            it('works when the filter is added later', function(){
+                // treeStore does npot have a filter yet
+                expect(treeStore.getCount()).to.be(1);
+                expect(olMap.getLayers().getLength()).to.be(1);
+
+                // add layer that should be added to both
+                olMap.addLayer(new ol.layer.Vector());
+
+                expect(treeStore.getCount()).to.be(2);
+                expect(olMap.getLayers().getLength()).to.be(2);
+
+                // now add the filter to the stzre
+                treeStore.addFilter([
+                    function(rec){
+                        return !(rec.data instanceof ol.layer.Vector);
+                    }
+                ]);
+
+                expect(treeStore.getCount()).to.be(1);
+                expect(olMap.getLayers().getLength()).to.be(2);
+            });
+
+        });
 
     });
 });
