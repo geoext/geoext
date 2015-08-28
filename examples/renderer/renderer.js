f0d05ac284f20cb048790d6fbfeed928fae03253
Marc Jansen <jansen@terrestris.de>
2015-08-28 12:05:26 +0200

    Fix linting issues of feature renderer example

--- examples/renderer/renderer.js
+++ examples/renderer/renderer.js
@@ -2,13 +2,13 @@
 var redStroke = new ol.style.Stroke({color: 'red', width: 2});
 var redFill = new ol.style.Fill({color: [255, 0, 0, 0.25]});
 var red = new ol.style.Style({
-  stroke: redStroke,
-  fill: redFill,
-  image: new ol.style.Circle({
     stroke: redStroke,
     fill: redFill,
-    radius: 5
-  })
+    image: new ol.style.Circle({
+        stroke: redStroke,
+        fill: redFill,
+        radius: 5
+    })
 });
 
 var custom = {
@@ -23,11 +23,20 @@ var custom = {
         })
     }),
     line: new ol.style.Style({
-        stroke: new ol.style.Stroke({color: "#669900", width: 3})
+        stroke: new ol.style.Stroke({
+            color: "#669900",
+            width: 3
+        })
     }),
     poly: new ol.style.Style({
-        fill: new ol.style.Fill({color: [128, 128, 0, 0.25]}),
-        stroke: new ol.style.Stroke({color: "#666666", width: 2, lineDash: [1.5, 7.5]})
+        fill: new ol.style.Fill({
+            color: [128, 128, 0, 0.25]
+        }),
+        stroke: new ol.style.Stroke({
+            color: "#666666",
+            width: 2,
+            lineDash: [1.5, 7.5]
+        })
     }),
     text: new ol.style.Style({
         text: new ol.style.Text({
@@ -38,46 +47,84 @@ var custom = {
 };
 
 var stacked = {
-    point: [new ol.style.Style({
-        image: new ol.style.Circle({
-            radius: 8,
-            fill: new ol.style.Fill({color: "white"}),
-            stroke: new ol.style.Stroke({color: "red", width: 2})
+    point: [
+        new ol.style.Style({
+            image: new ol.style.Circle({
+                radius: 8,
+                fill: new ol.style.Fill({
+                    color: "white"
+                }),
+                stroke: new ol.style.Stroke({
+                    color: "red", width: 2
+                })
+            })
+        }),
+        new ol.style.Style({
+            image: new ol.style.RegularShape({
+                fill: new ol.style.Fill({
+                    color: "red"
+                }),
+                stroke: new ol.style.Stroke({
+                    color: "black"
+                }),
+                points: 5,
+                radius: 5,
+                radius2: 2,
+                angle: 0
+            })
         })
-    }), new ol.style.Style({
-        image: new ol.style.RegularShape({
-            fill: new ol.style.Fill({color: "red"}),
-            stroke: new ol.style.Stroke({color: "black"}),
-            points: 5,
-            radius: 5,
-            radius2: 2,
-            angle: 0
+    ],
+    line: [
+        new ol.style.Style({
+            stroke: new ol.style.Stroke({
+                color: "red", width: 5
+            })
+        }),
+        new ol.style.Style({
+            stroke: new ol.style.Stroke({
+                color: "#ff9933",
+                width: 2
+            })
         })
-    })],
-    line: [new ol.style.Style({
-        stroke: new ol.style.Stroke({color: "red", width: 5})
-    }), new ol.style.Style({
-        stroke: new ol.style.Stroke({color: "#ff9933", width: 2})
-    })],
-    poly: [new ol.style.Style({
-        stroke: new ol.style.Stroke({color: "#669900", width: 3}),
-        fill: new ol.style.Fill({color: "white"})
-    }), new ol.style.Style({
-        stroke: new ol.style.Stroke({color: "red", width: 2, lineDash: [1.5, 7.5]})
-    })],
-    text: [new ol.style.Style({
-        text: new ol.style.Text({
-            fill: new ol.style.Fill({color: "#FF0000"}),
-            font: '18px Helvetica',
-            text: 'Ab'
+    ],
+    poly: [
+        new ol.style.Style({
+            stroke: new ol.style.Stroke({
+                color: "#669900",
+                width: 3
+            }),
+            fill: new ol.style.Fill({
+                color: "white"
+            })
+        }),
+        new ol.style.Style({
+            stroke: new ol.style.Stroke({
+                color: "red",
+                width: 2,
+                lineDash: [1.5, 7.5]
+            })
         })
-    }), new ol.style.Style({
-        text: new ol.style.Text({
-            fill: new ol.style.Fill({color: "#00FF00"}),
-            text: 'Ab',
-            font: '12px Helvetica'
+    ],
+    text: [
+        new ol.style.Style({
+            text: new ol.style.Text({
+                fill: new ol.style.Fill({
+                    color: "#FF0000"
+                }),
+                font: '18px Helvetica',
+                text: 'Ab'
+            })
+        }),
+        new ol.style.Style({
+            text: new ol.style.Text({
+                fill: new ol.style.Fill({
+                    color: "#00FF00"
+                }),
+                text: 'Ab',
+                font: '12px Helvetica'
+            })
         })
-    })]
+    ]
 };
 
 var graphicText = {
@@ -154,23 +201,91 @@ function render() {
 Ext.application({
     name: 'FeatureRenderer GeoExt2',
     launch: function() {
-       Ext.create("GeoExt.component.FeatureRenderer", {renderTo: 'line_default', symbolType: "Line"});
-       Ext.create("GeoExt.component.FeatureRenderer", {renderTo: 'line_red', symbolizers: red, symbolType: "Line"});
-       Ext.create("GeoExt.component.FeatureRenderer", {renderTo: 'point_default', symbolType: "Point"});
-       Ext.create("GeoExt.component.FeatureRenderer", {renderTo: 'point_red', symbolizers: red, symbolType: "Point"});
-       Ext.create("GeoExt.component.FeatureRenderer", {renderTo: 'poly_default', symbolType: "Polygon"});
-       Ext.create("GeoExt.component.FeatureRenderer", {renderTo: 'poly_red', symbolizers: red, symbolType: "Polygon"});
-       Ext.create("GeoExt.component.FeatureRenderer", {renderTo: 'line_custom', symbolizers: custom.line, symbolType: "Line"});
-       Ext.create("GeoExt.component.FeatureRenderer", {renderTo: 'point_custom', symbolizers: custom.point, symbolType: "Point"});
-       Ext.create("GeoExt.component.FeatureRenderer", {renderTo: 'poly_custom', symbolizers: custom.poly, symbolType: "Polygon"});
-       Ext.create("GeoExt.component.FeatureRenderer", {renderTo: 'text_custom', symbolizers: custom.text, symbolType: "Text"});
-       Ext.create("GeoExt.component.FeatureRenderer", {renderTo: 'line_stacked', symbolizers: stacked.line, symbolType: "Line"});
-       Ext.create("GeoExt.component.FeatureRenderer", {renderTo: 'point_stacked', symbolizers: stacked.point, symbolType: "Point"});
-       Ext.create("GeoExt.component.FeatureRenderer", {renderTo: 'poly_stacked', symbolizers: stacked.poly, symbolType: "Polygon"});
-       Ext.create("GeoExt.component.FeatureRenderer", {renderTo: 'text_stacked', symbolizers: stacked.text, symbolType: "Text"});
-       Ext.create("GeoExt.component.FeatureRenderer", {renderTo: 'text-only', symbolizers: graphicText.text, symbolType: "Text"});
-       Ext.create("GeoExt.component.FeatureRenderer", {renderTo: 'graphic-only', symbolizers: graphicText.graphic, symbolType: "Text"});
-       Ext.create("GeoExt.component.FeatureRenderer", {renderTo: 'text-graphic', symbolizers: [graphicText.text, graphicText.graphic], symbolType: "Text"});
-       Ext.get("render").dom.onclick = render;
+        Ext.create("GeoExt.component.FeatureRenderer", {
+            renderTo: 'line_default',
+            symbolType: "Line"
+        });
+        Ext.create("GeoExt.component.FeatureRenderer", {
+            renderTo: 'line_red',
+            symbolizers: red,
+            symbolType: "Line"
+        });
+        Ext.create("GeoExt.component.FeatureRenderer", {
+            renderTo: 'point_default',
+            symbolType: "Point"
+        });
+        Ext.create("GeoExt.component.FeatureRenderer", {
+            renderTo: 'point_red',
+            symbolizers: red,
+            symbolType: "Point"
+        });
+        Ext.create("GeoExt.component.FeatureRenderer", {
+            renderTo: 'poly_default',
+            symbolType: "Polygon"
+        });
+        Ext.create("GeoExt.component.FeatureRenderer", {
+            renderTo: 'poly_red',
+            symbolizers: red,
+            symbolType: "Polygon"
+        });
+        Ext.create("GeoExt.component.FeatureRenderer", {
+            renderTo: 'line_custom',
+            symbolizers: custom.line,
+            symbolType: "Line"
+        });
+        Ext.create("GeoExt.component.FeatureRenderer", {
+            renderTo: 'point_custom',
+            symbolizers: custom.point,
+            symbolType: "Point"
+        });
+        Ext.create("GeoExt.component.FeatureRenderer", {
+            renderTo: 'poly_custom',
+            symbolizers: custom.poly,
+            symbolType: "Polygon"
+        });
+        Ext.create("GeoExt.component.FeatureRenderer", {
+            renderTo: 'text_custom',
+            symbolizers: custom.text,
+            symbolType: "Text"
+        });
+        Ext.create("GeoExt.component.FeatureRenderer", {
+            renderTo: 'line_stacked',
+            symbolizers: stacked.line,
+            symbolType: "Line"
+        });
+        Ext.create("GeoExt.component.FeatureRenderer", {
+            renderTo: 'point_stacked',
+            symbolizers: stacked.point,
+            symbolType: "Point"
+        });
+        Ext.create("GeoExt.component.FeatureRenderer", {
+            renderTo: 'poly_stacked',
+            symbolizers: stacked.poly,
+            symbolType: "Polygon"
+        });
+        Ext.create("GeoExt.component.FeatureRenderer", {
+            renderTo: 'text_stacked',
+            symbolizers: stacked.text,
+            symbolType: "Text"
+        });
+        Ext.create("GeoExt.component.FeatureRenderer", {
+            renderTo: 'text-only',
+            symbolizers: graphicText.text,
+            symbolType: "Text"
+        });
+        Ext.create("GeoExt.component.FeatureRenderer", {
+            renderTo: 'graphic-only',
+            symbolizers: graphicText.graphic,
+            symbolType: "Text"
+        });
+        Ext.create("GeoExt.component.FeatureRenderer", {
+            renderTo: 'text-graphic',
+            symbolizers: [
+                graphicText.text,
+                graphicText.graphic
+            ],
+            symbolType: "Text"
+        });
+        Ext.get("render").dom.onclick = render;
     }
 });
