015951b0e2bbea9ecaa3a08c7ef76d724dc7cf6e
Marc Jansen <jansen@terrestris.de>
2015-08-28 11:16:36 +0200

    Fix linting issue in Base serializer

--- src/data/serializer/Base.js
+++ src/data/serializer/Base.js
@@ -13,10 +13,10 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
- /**
-  * The base class for all serializers.
-  */
- Ext.define('GeoExt.data.serializer.Base', {
+/**
+ * The base class for all serializers.
+ */
+Ext.define('GeoExt.data.serializer.Base', {
     extend: 'Ext.Base',
     requires: 'GeoExt.data.MapfishPrintProvider',
     inheritableStatics: {
