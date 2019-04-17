b2c9bf89868064da5b120521ae5192cc1b2a6adc
Christian Mayer <chris@meggsimum.de>
2019-04-17 15:38:08 +0200

    Add copyright header to ContextMenu plugin

--- src/plugin/layertreenode/ContextMenu.js
+++ src/plugin/layertreenode/ContextMenu.js
@@ -1,4 +1,18 @@
-
+/* Copyright (c) 2015-2019 The Open Source Geospatial Foundation
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 /**
  * (Abstract) plugin for an Ext.tree.Column used in an layer tree in order to
  * render a custom UI component on right-click of a LayerTreeNode, e. g. a menu
