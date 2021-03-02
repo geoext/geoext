00ae4be9547529328c4ece870a71897f8ce05767
Andreas Schmitz <schmitz@terrestris.de>
2021-03-02 14:24:33 +0100

    Fix ol3 tree node mode (#665)
    * Fix ol3 tree node mode
    
    * Turn condition around

--- src/data/model/LayerTreeNode.js
+++ src/data/model/LayerTreeNode.js
@@ -115,6 +115,10 @@ Ext.define('GeoExt.data.model.LayerTreeNode', {
 
         // forward changes to ol object
         if (key === 'checked') {
+            if (!classicMode && me.data.checked === newValue) {
+                me.getOlLayer().set('visible', newValue);
+                return;
+            }
             me.__updating = true;
             if (me.get('isLayerGroup') && classicMode) {
                 me.getOlLayer().set('visible', newValue);
