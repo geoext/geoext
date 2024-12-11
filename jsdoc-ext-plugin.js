e494c7ef5f60917d5a56fa4091b289e5c37c7bf5
sethg <sethg@geographika.co.uk>
2024-12-11 16:08:05 +0100

    Add a custom plugin for working with ExtJS classes and JSDoc

---
+++ jsdoc-ext-plugin.js
@@ -0,0 +1,23 @@
+exports.handlers = {
+    /**
+     * Intercepts and pre-processes the source code before JSDoc parses it.
+     * Extracts the object passed to `Ext.define` and replaces it with
+     * a "class-like" syntax for JSDoc to parse.
+     */
+    beforeParse(e) {
+        const classRegex = /Ext\.define\(['"]([^'"]+)['"],\s*(\{[\s\S]*.*\})\);/g;
+        console.log(e.filename);
+        // console.log(e.source);
+        e.source = e.source.replace(classRegex, (match, className, classBody) => {
+
+            //console.log(className);
+            //console.log(classBody);
+
+            const extractedClass = `${className} = ${classBody};`;
+
+            // Log the transformation for debugging
+            // console.log(extractedClass);
+            return extractedClass;
+        });
+    },
+};
